39ed709e607c3b3cbfc5053fda7ec9bfda61fb73
FlameyosFlow <49708496+coderFlameyosFlow@users.noreply.github.com>
2023-05-03 20:04:30 +0300

    Delete common directory

--- common/src/main/java/io/github/flameware/common/CustomSenderFactory.java
+++
@@ -1,5 +0,0 @@
-package io.github.flameware.common;
-
-public interface CustomSenderFactory<Sender, PlatformSender> {
-    Sender createCustomSender(PlatformSender sender);
-}
--- common/src/main/java/io/github/flameware/common/exceptions/SenderNotPlayerException.java
+++
@@ -1,9 +0,0 @@
-package io.github.flameware.common.exceptions;
-
-import org.jetbrains.annotations.Nullable;
-
-public class SenderNotPlayerException extends Exception {
-    public SenderNotPlayerException(@Nullable String message) {
-        super(message == null ? "Sender expected to be player but found console" : message);
-    }
-}
